324cee48b6edfd94f9a1f06ef16d7057fa5cbda9
Zxilly <zhouxinyu1001@gmail.com>
2021-09-03 01:56:20 +0800

    refactor: remove log

--- app/src/main/java/top/learningman/mipush/MiPushReceiver.kt
+++ app/src/main/java/top/learningman/mipush/MiPushReceiver.kt
@@ -6,13 +6,7 @@ import android.content.Intent
 import android.util.Log
 import com.xiaomi.mipush.sdk.MiPushCommandMessage
 import com.xiaomi.mipush.sdk.PushMessageReceiver
+import kotlin.concurrent.thread
 
 class MiPushReceiver : PushMessageReceiver() {
-
-    override fun onReceiveRegisterResult(context: Context, msg: MiPushCommandMessage) {
-        val command = msg.command
-        val args = msg.commandArguments
-        Log.i("Command", command)
-        Log.i("Args", args.toString())
-    }
 }
