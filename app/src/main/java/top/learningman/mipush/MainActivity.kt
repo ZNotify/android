aa454e41406092e9f6dffb5be554222bc204c20e
Zxilly <zhouxinyu1001@gmail.com>
2021-09-02 20:17:26 +0800

    feat: add UI

--- app/src/main/java/top/learningman/mipush/MainActivity.kt
+++ app/src/main/java/top/learningman/mipush/MainActivity.kt
@@ -3,11 +3,15 @@ package top.learningman.mipush
 import android.app.ActivityManager
 import android.content.Context
 import android.content.Intent
+import android.content.res.ColorStateList
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Process
+import androidx.core.content.ContextCompat
+import androidx.preference.PreferenceManager
 import com.xiaomi.mipush.sdk.MiPushClient
 import kotlinx.android.synthetic.main.activity_main.*
+import net.steamcrafted.materialiconlib.MaterialDrawableBuilder
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -19,6 +23,15 @@ class MainActivity : AppCompatActivity() {
             val intent = Intent(this, SettingsActivity::class.java)
             startActivity(intent)
         }
+
+        val pref = PreferenceManager.getDefaultSharedPreferences(this)
+        val userid = pref.getString("user_id", "none")!!
+        if (userid == "none") {
+            reg_status.setCardBackgroundColor(this.colorList(R.color.reg_failed))
+            reg_status_text.text = getString(R.string.not_set_userid_err)
+            reg_status_icon.setIcon(MaterialDrawableBuilder.IconValue.ALERT_CIRCLE_OUTLINE)
+        }
+
 
         if (shouldInit()) {
             MiPushClient.registerPush(this, "2882303761520035342", "5272003587342")
@@ -36,4 +49,8 @@ class MainActivity : AppCompatActivity() {
         }
         return false
     }
+}
+
+fun Context.colorList(id: Int): ColorStateList {
+    return ColorStateList.valueOf(ContextCompat.getColor(this, id))
 }
