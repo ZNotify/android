7f188ce59fa824d3a2fecc6b0c2a473f2ea38755
Zxilly <zhouxinyu1001@gmail.com>
2021-09-08 17:31:15 +0800

    feat: add click event

---
+++ app/src/main/java/top/learningman/mipush/NotificationViewActivity.kt
@@ -0,0 +1,10 @@
+package top.learningman.mipush
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class NotificationViewActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
