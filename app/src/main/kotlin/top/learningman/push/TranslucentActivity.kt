a7224b0770d5555dfae0be35a823019711f7ea62
Zxilly <zxilly@outlook.com>
2022-06-19 15:25:55 +0800

    refactor: rename fields
    Signed-off-by: Zxilly <zxilly@outlook.com>

--- app/src/main/kotlin/top/learningman/push/TranslucentActivity.kt
+++ app/src/main/kotlin/top/learningman/push/TranslucentActivity.kt
@@ -2,6 +2,7 @@ package top.learningman.push
 
 import android.os.Bundle
 import android.util.Log
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import top.learningman.push.databinding.ActivityTranslucentBinding
 import top.learningman.push.entity.MessageAdapter.MessageHolder.Companion.fromRFC3339
@@ -15,26 +16,30 @@ class TranslucentActivity : AppCompatActivity() {
         val binding = ActivityTranslucentBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        val userID = intent.getStringExtra("userID")!!
-        val long = intent.getStringExtra("long")!!
-        val msgID = intent.getStringExtra("msgID")!!
-        val title = intent.getStringExtra("title")!!
-        val content = intent.getStringExtra("content")!!
-        val time = intent.getStringExtra("createdAt")!!.fromRFC3339()
-        Log.d(
-            "TranslucentActivity",
-            "userID: $userID, long: $long, msgID: $msgID, title: $title, content: $content, time: ${
-                intent.getStringExtra(
-                    "createdAt"
-                )
-            }"
-        )
+        val userID = intent.getStringExtra("user_id")
+        val long = intent.getStringExtra("long")
+        val msgID = intent.getStringExtra("msg_id")
+        val title = intent.getStringExtra("title")
+        val content = intent.getStringExtra("content")
+        val time = intent.getStringExtra("created_at")
+
+        if (userID == null
+            || long == null
+            || msgID == null
+            || title == null
+            || content == null
+            || time == null
+        ) {
+            Toast.makeText(this, "intent missing field", Toast.LENGTH_SHORT).show()
+            finish()
+            return
+        }
 
         val message = MessageDialog.Message(
             title,
             content,
             long,
-            time,
+            time.fromRFC3339(),
             msgID,
             userID
         )
