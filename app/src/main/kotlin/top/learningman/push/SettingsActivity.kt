047bbd26666b228ce2ae36e9bb87a8be13cf869c
Zxilly <zxilly@outlook.com>
2022-09-10 16:14:50 +0800

    feat: add update check
    Signed-off-by: Zxilly <zxilly@outlook.com>

--- app/src/main/kotlin/top/learningman/push/SettingsActivity.kt
+++ app/src/main/kotlin/top/learningman/push/SettingsActivity.kt
@@ -1,8 +1,13 @@
 package top.learningman.push
 
+import android.content.Context
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.preference.Preference
 import androidx.preference.PreferenceFragmentCompat
+import com.microsoft.appcenter.distribute.Distribute
 import top.learningman.push.databinding.SettingsActivityBinding
 
 class SettingsActivity : AppCompatActivity() {
@@ -21,8 +26,23 @@ class SettingsActivity : AppCompatActivity() {
     }
 
     class SettingsFragment : PreferenceFragmentCompat() {
+        override fun onAttach(context: Context) {
+            super.onAttach(context)
+            Log.d("SettingsActivity", "onAttach")
+        }
+
         override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
             setPreferencesFromResource(R.xml.root_preferences, rootKey)
+
+            findPreference<Preference>("version")?.apply {
+                Log.d("SettingsActivity", "Version Clicked")
+                setOnPreferenceClickListener {
+                    Distribute.checkForUpdate()
+                    Toast.makeText(context, "Checking for updates...", Toast.LENGTH_SHORT).show()
+                    true
+                }
+                summary = "Version ${BuildConfig.VERSION_NAME} (${BuildConfig.VERSION_CODE})"
+            }
         }
     }
 }
