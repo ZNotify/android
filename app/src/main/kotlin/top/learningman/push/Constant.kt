61b315fd419873934535286661275123dc7d4f58
Zxilly <zxilly@outlook.com>
2022-09-27 21:45:28 +0800

    fix: fix api endpoint

--- app/src/main/kotlin/top/learningman/push/Constant.kt
+++ app/src/main/kotlin/top/learningman/push/Constant.kt
@@ -1,9 +1,9 @@
 package top.learningman.push
 
 object Constant {
-    private val HOST = if (!BuildConfig.DEBUG) "192.168.1.103:14444" else "push.learningman.top"
+    private val HOST = if (BuildConfig.DEBUG) "192.168.1.103:14444" else "push.learningman.top"
 
     const val APP_CENTER_SECRET = "0c045975-212b-441d-9ee4-e6ab9c76f8a3"
-    val API_ENDPOINT = if (!BuildConfig.DEBUG) "http://$HOST" else "https://$HOST"
-    val API_WS_ENDPOINT = if (!BuildConfig.DEBUG) "ws://$HOST" else "wss://$HOST"
+    val API_ENDPOINT = if (BuildConfig.DEBUG) "http://$HOST" else "https://$HOST"
+    val API_WS_ENDPOINT = if (BuildConfig.DEBUG) "ws://$HOST" else "wss://$HOST"
 }
