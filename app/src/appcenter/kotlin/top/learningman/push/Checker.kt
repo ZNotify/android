b8277b577e6637a4b5e6e617de9d8039b51452d3
Zxilly <zxilly@outlook.com>
2022-10-07 21:12:09 +0800

    fix: compile

--- app/src/appcenter/kotlin/top/learningman/push/Checker.kt
+++ app/src/appcenter/kotlin/top/learningman/push/Checker.kt
@@ -1,5 +1,6 @@
 package top.learningman.push
 
+import android.content.Context
 import dev.zxilly.lib.upgrader.checker.AppCenterChecker
 
 internal val checker = AppCenterChecker("0c045975-212b-441d-9ee4-e6ab9c76f8a3")
