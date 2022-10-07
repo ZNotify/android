f9cd59bab1036ccf36f4f97237d9d378f4102ea4
Zxilly <zxilly@outlook.com>
2022-10-07 22:58:24 +0800

    ci: use token

--- scripts/appcenter.sh
+++ scripts/appcenter.sh
@@ -7,10 +7,12 @@ appcenter distribute release \
   --app "$app" \
   --group "public" \
   --file "app/build/outputs/apk/appcenter/release/app-appcenter-release.apk" \
-  --release-notes "$RELEASE_NOTES"
+  --release-notes "$RELEASE_NOTES" \
+  --token "$APPCENTER_TOKEN"
 
 appcenter crashes upload-mappings \
   --app "zxilly/Notify" \
   --version-code "$VERSION_CODE" \
   --version-name "$VERSION_NAME" \
-  --mapping "app/build/outputs/mapping/appcenterRelease/mapping.txt"
+  --mapping "app/build/outputs/mapping/appcenterRelease/mapping.txt" \
+  --token "$APPCENTER_TOKEN"
