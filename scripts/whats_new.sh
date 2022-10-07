a8581deecca20f9cfd9e5c17b5f276d9379c5ed9
Zxilly <zxilly@outlook.com>
2022-10-08 02:17:44 +0800

    ci: fix lang
    [release: 1.0.2]

--- scripts/whats_new.sh
+++ scripts/whats_new.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 mkdir whatsNew
 echo "请参阅 https://github.com/ZNotify/android/releases/tag/$VERSION" > whatsNew/whatsnew-zh-CN.txt
-echo "Please refer to https://github.com/ZNotify/android/releases/tag/$VERSION" > whatsNew/whatsnew-en.txt
+echo "Please refer to https://github.com/ZNotify/android/releases/tag/$VERSION" > whatsNew/whatsnew-en-US.txt
