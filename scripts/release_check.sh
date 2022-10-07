a511a480ed08f39bd5fdc601376f1d0641a054b2
Zxilly <zxilly@outlook.com>
2022-10-08 02:04:07 +0800

    ci: also release on play store
    [release: 1.0.1]

--- scripts/release_check.sh
+++ scripts/release_check.sh
@@ -3,10 +3,21 @@
 # get last git commit message
 git_commit_message=$(git log -1 --pretty=%B)
 
-#if commit message contains "[release]" then set output
-if [[ $git_commit_message == *"[release]"* ]]; then
+if [[ $git_commit_message == *"[release:"* ]]; then
     echo "::set-output name=release::true"
     echo "Should release"
+    # get version from [release: x.x.x]
+    version=$(echo "$git_commit_message" | grep -o "\[release:.*\]" | sed "s/\[release:\s*//g" | sed "s/\s*\]//g")
+    if [[ $version =~ ^[0-9]+\.[0-9]+\.[0-9]+$ ]]; then
+        echo "::set-output name=version::$version"
+        echo "Version is $version"
+    else
+        echo "Version format is not correct"
+        echo "Version format should be x.x.x"
+        echo "But got $version"
+        exit 1
+    fi
+
 else
     echo "::set-output name=release::false"
     echo "Should not release"
