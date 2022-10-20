4cb74c69a1b5243fbc232a8d6051521cafdf0177
Zxilly <zxilly@outlook.com>
2022-10-20 21:49:01 +0800

    style: format

--- app/src/main/kotlin/top/learningman/push/Constant.kt
+++ app/src/main/kotlin/top/learningman/push/Constant.kt
@@ -4,39 +4,40 @@ object Constant {
     private val ENDPOINT_LIST = arrayOf("push.learningman.top", "192.168.1.111:14444")
     private const val ENDPOINT_INDEX = 1
 
-    private val HOST:String
-    get() {
-        if (BuildConfig.DEBUG){
-            return ENDPOINT_LIST[ENDPOINT_INDEX]
+    private val HOST: String
+        get() {
+            if (BuildConfig.DEBUG) {
+                return ENDPOINT_LIST[ENDPOINT_INDEX]
+            }
+            return ENDPOINT_LIST[0]
         }
-        return ENDPOINT_LIST[0]
-    }
 
     private val USE_SECURE_PROTOCOL: Boolean
-    get() {
-        if (BuildConfig.DEBUG){
-            return false
+        get() {
+            if (BuildConfig.DEBUG) {
+                return false
+            }
+            return true
         }
-        return true
-    }
 
-    private val HTTP_PROTOCOL:String
-    get() {
-        if (USE_SECURE_PROTOCOL){
-            return "https://"
+    private val HTTP_PROTOCOL: String
+        get() {
+            if (USE_SECURE_PROTOCOL) {
+                return "https://"
+            }
+            return "http://"
         }
-        return "http://"
-    }
 
-    private val WEBSOCKET_PROTOCOL:String
-    get() {
-        if (USE_SECURE_PROTOCOL){
-            return "wss://"
+    private val WEBSOCKET_PROTOCOL: String
+        get() {
+            if (USE_SECURE_PROTOCOL) {
+                return "wss://"
+            }
+            return "ws://"
         }
-        return "ws://"
-    }
 
     const val APP_CENTER_SECRET = "0c045975-212b-441d-9ee4-e6ab9c76f8a3"
+
     val API_ENDPOINT = "${HTTP_PROTOCOL}${HOST}"
     val API_WS_ENDPOINT = "${WEBSOCKET_PROTOCOL}${HOST}"
 }
